2568437560a8f295e73c8b31b4ff294d9f1cf066
roddy-reyess <sergireyes26@gmail.com>
2019-10-01 19:56:25 +0200

    af

--- Practica2_SergiReyes/test_llistes.py
+++ Practica2_SergiReyes/test_llistes.py
@@ -1,11 +1,21 @@
 
 import random
-
 def llistes_rand():
     novaLlista = []
     for x in range(10):
         novaLlista.append(random.randint(1,100))
     return novaLlista
 
+
+def llistes_rand2():
+    novaLlista = []
+    for x in range(100):
+        novaLlista.append(random.randint(1,100))
+    return novaLlista
+
 prova1 = llistes_rand()
 print(prova1)
+print(prova1[-1])
+prova2 =  llistes_rand2()
+print(prova2)
+print(prova2[-1])
