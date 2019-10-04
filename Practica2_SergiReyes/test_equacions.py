f3bfd41492147503c63ec4c7ef4153b3e4305122
roddy-reyess <sergireyes26@gmail.com>
2019-10-04 16:39:52 +0200

    cleared

---
+++ Practica2_SergiReyes/test_equacions.py
@@ -0,0 +1,10 @@
+from equacio1 import equacio1
+eq1 = equacio1("20x + 30 = 70")
+respuesta = eq1.calcula()
+print(respuesta)
+eq2 = equacio1("20x - 70 = 30")
+respuesta = eq2.calcula()
+print(respuesta)
+eq3 = equacio1("3.4x - 7.8 = 4.3")
+respuesta = eq3.calcula()
+print(respuesta)
