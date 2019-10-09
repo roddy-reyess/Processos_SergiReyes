775970316272c6a41c44ce699dfbfa87d96a73f3
roddy-reyess <sergireyes26@gmail.com>
2019-10-09 19:16:36 +0200

    done!!!

--- Practica3_SergiReyes/equacio1.py
+++ Practica3_SergiReyes/equacio1.py
@@ -1,15 +1,37 @@
 class equacio1:
     def __init__(self, equacio):
         self.s_eq = equacio
+        self.calculante = self.s_eq.split()
+        self.tuX = 0.0
+        self.num2 = 0.0
+        self.finalnum = 0.0
+        self.respuesta = 0.0
     def calcula(self):
-        calculante = self.s_eq.split()
-        tuX = calculante[0]
-        tuX = tuX.replace(tuX[-1],'')
-        tuX = float(tuX)
-        finalnum = float(calculante[-1])
-        num2 = float(calculante[2])
-        if calculante[1] == "+":
-            respuesta = (finalnum - num2) / tuX
-        elif calculante[1] == "-":
-            respuesta = (finalnum + num2) / tuX
-        return "x " + "= " + str(respuesta)
+        if("x" in self.calculante[0]):
+
+            self.tuX = self.calculante[0]
+            self.tuX = self.tuX.replace(self.tuX[-1],'')
+            self.tuX = float(self.tuX)
+
+            self.finalnum = float(self.calculante[-1])
+
+            try:
+                float(self.calculante[2])
+            except:
+                return "l'equacio conte caracter no calculables: "+self.s_eq
+
+            self.num2 = float(self.calculante[2])
+
+            if self.calculante[1] == "+":
+                self.respuesta = (self.finalnum - self.num2) / self.tuX
+                self.respuesta = float("{0:.2f}".format(self.respuesta))
+
+            elif self.calculante[1] == "-":
+                self.respuesta = (self.finalnum + self.num2) / self.tuX
+                self.respuesta = float("{0:.2f}".format(self.respuesta))
+                
+            else:
+                self.respuesta = "Error " + self.calculante[1]
+            return self.respuesta
+        else:
+            return "l'equacio no segueix el format: ax + b = c"
---
+++ Practica3_SergiReyes/test_unitari.py
@@ -0,0 +1,31 @@
+import unittest
+from equacio1 import equacio1
+
+class test_unitari(unittest.TestCase):
+
+    def test_resultat(self):
+        eq1 = equacio1("20x + 30 = 70")
+        self.assertEqual(eq1.calcula(), 2.0)
+
+    def test_Operador(self):
+        eq1 = equacio1("20x / 30 = 70")
+        self.assertEqual(eq1.calcula(), "Error " + eq1.calculante[1])
+        self.assertIsInstance(eq1.calculante[1], basestring)
+
+    def test_negatiu(self):
+        eq = equacio1("20x - 30 = 70")
+        self.assertEqual(eq.calcula(),5)
+
+    def test_float(self):
+        eq = equacio1("20x + 30.5 = 7.7")
+        self.assertEqual(eq.calcula(),-1.14)
+
+    def test_negatiu(self):
+        eq = equacio1("2x - p = 7")
+        self.assertEqual(eq.calcula(),"l'equacio conte caracter no calculables: "+eq.s_eq)
+        
+    def test_fromat_erroni(self):
+        eq = equacio1("3 - 2x = 7")
+        self.assertEqual(eq.calcula(),"l'equacio no segueix el format: ax + b = c")
+if __name__ == '__main__':
+    unittest.main()
