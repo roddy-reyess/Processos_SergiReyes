7ef8a7c28a09b8832859546ffee17ef155ab4cda
roddy-reyess <sergireyes26@gmail.com>
2019-12-17 19:53:15 +0100

    Practica complerta

--- Practica14_SergiReyes/task14.py
+++ Practica14_SergiReyes/task14.py
@@ -43,7 +43,50 @@ def inici():
     f.close()
     #print open('fitxer.txt', 'ro').read()
 
+def fill(s, child_conn):
+    while True:
+        x = child_conn.recv()
+        y = child_conn.recv()
+        if x.lower() == "q" or y.lower() == "q":
+            break
+        else:
+            substitueix(str(x),str(y), s)
+
+def is_int(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
 
 if __name__ == '__main__' :
-
     inici()
+    parent_conn, child_conn = Pipe()
+    s = Semaphore(1)
+    running = True
+    fill = Process(target=fill, args=(s, child_conn))
+    fill.start()
+    while running == True:
+        txt = raw_input("Quin nombre vols substituir? [nombres del 1 al 100] [q per sortir] ")
+        if txt.lower() == "q":
+            parent_conn.send(txt)
+            parent_conn.send("q")
+            time.sleep(1)
+            running = False
+        elif is_int(txt) == True:
+            txt2 = raw_input("Per quin nombre el vols editar? ")
+            if txt2.lower() == "q":
+                parent_conn.send(txt)
+                parent_conn.send(txt2)
+                time.sleep(1)
+                running = False
+            else:
+                if is_int(txt2) == True:
+                    parent_conn.send(txt)
+                    parent_conn.send(txt2)
+                    time.sleep(1)
+                else:
+                    print "No es un nombre vàlid"
+        elif is_int(txt) == False:
+            print "No es un nombre vàlid"
+    fill.join()
