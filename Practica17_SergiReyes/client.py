f2ccd00580c32add25998ba867aeaca53642f166
roddy-reyess <sergireyes26@gmail.com>
2020-02-18 19:12:46 +0100

    finished

--- Practica17_SergiReyes/client.py
+++ Practica17_SergiReyes/client.py
@@ -1,21 +1,114 @@
-# Echo client program
-import socket
-import messages
-from threading import Thread
+import thread
+import os
+from ChatFns import *
 
-HOST = 'localhost'    # The remote host
-PORT = 50007              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST,PORT))
+#---------------------------------------------------#
+#---------INITIALIZE CONNECTION VARIABLES-----------#
+#---------------------------------------------------#
+WindowTitle = 'JChat v0.1 - Client'
+HOST = 'localhost'
+PORT = 50007
+s = socket(AF_INET, SOCK_STREAM)
+name = ""
 
-thread = Thread(target = messages.send_message, args = (s, ))
-thread1 = Thread(target = messages.recv_message, args = (s, ))
 
-thread.daemon = True
 
-thread.start()
-thread1.start()
 
-thread1.join()
+#---------------------------------------------------#
+#------------------ MOUSE EVENTS -------------------#
+#---------------------------------------------------#
+def ClickAction():
+    #Write message to chat window
+    EntryText = FilteredMessage(EntryBox.get("0.0",END))
+    LoadMyEntry(ChatLog, EntryText)
 
-s.close()
+    #Scroll to the bottom of chat windows
+    ChatLog.yview(END)
+
+    #Erace previous message in Entry Box
+    EntryBox.delete("0.0",END)
+
+    #Send my mesage to all others
+    s.sendall(EntryText)
+    if EntryText[:-1].lower() == "bye":
+        s.close()
+        Tkinter.destroy()
+#---------------------------------------------------#
+#----------------- KEYBOARD EVENTS -----------------#
+#---------------------------------------------------#
+def PressAction(event):
+	EntryBox.config(state=NORMAL)
+	ClickAction()
+def DisableEntry(event):
+	EntryBox.config(state=DISABLED)
+
+
+#---------------------------------------------------#
+#-----------------GRAPHICS MANAGEMENT---------------#
+#---------------------------------------------------#
+
+#Create a window
+base = Tk()
+base.title(WindowTitle)
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+
+#Create a Chat window
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog.insert(END, "Connecting to your partner..\n")
+ChatLog.config(state=DISABLED)
+
+#Bind a scrollbar to the Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+#Create the Button to send message
+SendButton = Button(base, font=30, text="Send", width="12", height=5,
+                    bd=0, bg="#FFBF00", activebackground="#FACC2E",
+                    command=ClickAction)
+
+#Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+EntryBox.bind("<Return>", DisableEntry)
+EntryBox.bind("<KeyRelease-Return>", PressAction)
+
+#Place all components on the screen
+scrollbar.place(x=376,y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+
+
+#---------------------------------------------------#
+#----------------CONNECTION MANAGEMENT--------------#
+#---------------------------------------------------#
+
+def ReceiveData():
+    try:
+        s.connect((HOST, PORT))
+        LoadConnectionInfo(ChatLog, '[ Succesfully connected ]\n---------------------------------------------------------------')
+    except:
+        LoadConnectionInfo(ChatLog, '[ Unable to connect ]')
+        return
+
+    while 1:
+        try:
+            data = s.recv(1024)
+        except:
+            LoadConnectionInfo(ChatLog, '\n [ Your partner has disconnected ] \n')
+            break
+        if data != '':
+            uname = str(data).split(":")
+            LoadOtherEntry(ChatLog, uname[1], uname[0])
+            if base.focus_get() == None:
+                FlashMyWindow(WindowTitle)
+                playsound('notif.wav')
+
+        else:
+            LoadConnectionInfo(ChatLog, '\n [ Your partner has disconnected ] \n')
+            break
+    #s.close()
+
+thread.start_new_thread(ReceiveData,())
+
+base.mainloop()
--- Practica17_SergiReyes/server.py
+++ Practica17_SergiReyes/server.py
@@ -2,25 +2,71 @@
 import socket
 from threading import Thread
 import time
-import messages
-
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
+s.listen(3)
+global usernames
+usernames = []
+global connections
+connections = []
 
-thread = Thread(target = messages.send_message, args = (conn, ))
-thread1 = Thread(target = messages.recv_message, args = (conn, ))
+def add_Con():
 
-thread.daemon = True
+    while True:
+        user = ""
+        connections.append(s.accept())
+        if len(usernames) == 0:
+            connections[-1][0].sendto("Server :Insereix un nom d'usuari", connections[-1][1])
+            user = connections[-1][0].recv(1024)
+            usernames.append((user, connections[-1][1][1]))
+        else:
+            for client in connections:
+                for uname in usernames:
+                    if client[1][1] != uname[1]:
+                        client[0].sendto("Server :Insereix un nom d'usuari", client[1])
+                        user = client[0].recv(1024)
+                        usernames.append((user,  client[1][1]))
+
+        Thread(target = send_to, args = (connections[-1][0], connections[-1][1], user)).start()
+
+
+def send_to(conn, addr, uname):
+    while True:
+        message = conn.recv(1024)
+        print(message)
+        for client in connections:
+            if addr[1] != client[1][1]:
+                for username in usernames:
+                    if addr[1] == username[1]:
+                        client[0].sendto(str(username[0]+":"+message), addr)
+                        print(message + "    " + str(client[1]))
+            else:
+                print (str(client[1][1]) + "\n" + str(addr[1]))
+        if message[:-1].lower() == "bye":
+            for i in range(len(connections)):
+                if addr[1] == connections[i][1][1]:
+                    connections.pop(i)
+                    break
+            for i in range(len(usernames)):
+                if addr[1] == username[i][1]:
+                    usernames.pop(i)
+                    break
+            break
+
+thread = Thread(target = add_Con, args = ( ))
 
 thread.start()
-thread1.start()
 
-thread1.join()
+
+
+
+
+
+
 
 time.sleep(1)
+thread.join()
 s.close()
